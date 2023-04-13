1c84fda612f3e38012764e8444f65725c65e10d5
Leon <LAnneken09@gmail.com>
2023-04-13 19:20:41 +0200

    Changed Discord Link

--- Seiten/Public/Index/script.js
+++ Seiten/Public/Index/script.js
@@ -1,3 +1,3 @@
 document.getElementById("login").addEventListener("click", (ev) => {
-    window.location.href = "https://discord.com/api/oauth2/authorize?client_id=1094662577742696479&redirect_uri=https%3A%2F%2Feasytickets-dashboard.leontm-official.repl.co&response_type=token&scope=identify%20guilds%20guilds.members.read%20guilds.join";
+    window.location.href = "https://discord.com/api/oauth2/authorize?client_id=1094662577742696479&redirect_uri=https%3A%2F%2Feasytickets-dashboard.leontm-official.repl.co%2Fconnected&response_type=token&scope=identify%20guilds%20guilds.join";
 });
